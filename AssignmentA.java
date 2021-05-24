a6c7b2b08d1136d692efa86dc7322c6b211ebc55
Burmau G arba <Burmaug@gmail.com>
2021-05-24 16:26:40 -0400

    this is the assignment beginning for the file.

---
+++ AssignmentA.java
@@ -0,0 +1,33 @@
+package csc360_summer;
+
+import java.util.Scanner;
+
+public class AssignmentA
+{
+    public static void main(String[] args) throws Exception
+    {
+        Scanner scan = new Scanner(System.in);
+        //Get the file name from the user.
+        String filename = scan.next();
+        int result = 0;
+        //make file
+        java.io.File file = new java.io.File(filename);
+        //check if the file exist or not
+        if (!file.exists())
+        {
+            System.out.print("Could not find file: " + filename);
+            System.exit(1); //exit because the file doesnt exist
+        }
+        //read the file and make calculations
+        try(java.io.PrintWriter write= new java.io.PrintWriter(file);
+            Scanner input = new Scanner(file))
+        {
+            while (input.hasNext())
+            {
+                int number = input.nextInt();
+                result += number;
+            }
+        }
+        System.out.println(result);
+    }
+}
---
+++ AssignmentB.java
@@ -0,0 +1,7 @@
+package csc360_summer;
+
+public class AssignmentB {
+    public static void main(String[] args) {
+
+    }
+}
